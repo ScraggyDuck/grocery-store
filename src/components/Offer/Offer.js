8308e5031fad7d26b55a81d62a7afadd815dc7ef
JokerV <Quocvietcoi2000@gmail.com>
2020-08-15 23:04:36 +0700

    top-menu

---
+++ src/components/Offer/Offer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Offer.scss';
+export default function Offer() {
+  return (
+    <div className='row px-5 py-4 offer-section d-none d-lg-flex m-0'>
+      <div className='col-4'>
+        <img
+          className='img-fluid'
+          src='https://shop.redq.now.sh/_next/static/images/offer-2-90d3534e1ad62a8b8a977f1290e61e9f.png'
+          alt='offer-1'
+        />
+      </div>
+      <div className='col-4'>
+        <img
+          className='img-fluid'
+          src='https://shop.redq.now.sh/_next/static/images/offer-1-1f7a4c9ea0ba5a216bc7af1f60d044e0.png'
+          alt='offer-2'
+        />
+      </div>
+      <div className='col-4'>
+        <img
+          className='img-fluid'
+          src='https://shop.redq.now.sh/_next/static/images/offer-3-2f8285b13bef950f843cb4147666af6e.png'
+          alt='offer-3'
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Products from '../../components/Products/Products';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import Offer from '../../components/Offer/Offer';
 
 import './Home.scss';
 
@@ -11,6 +12,7 @@ export default function Home() {
     <div>
       <Banner />
       <div className='container-fluid p-0 main'>
+        <Offer />
         <div className='row m-0 p-0'>
           <div className='col-xl-2 col-lg-3 col-md-4 p-0'>
             <Sidebar />
